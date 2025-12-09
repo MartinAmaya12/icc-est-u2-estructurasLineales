3a18aaa61259901630e5481142eef8d617e268c0
Andrea Hurtado <ahurtadot1@est.ups.edu.ec>
2025-12-09 08:59:43 -0500

    Practica nodos

--- src/App.java
+++ src/App.java
@@ -1,16 +1,30 @@
+import controller.Stack;
 import models.Node;
 import models.Person;
-
 public class App {
     public static void main(String[] args) throws Exception {
+        
         Node node1 = new Node<Person>(new Person("Juan", 30));
         Node node2 = new Node<Person>(new Person("Maria", 35));
-        Node node3 = new Node<Person>(new Person("Pepito", 20));
-        Node node4 = new Node<Person>(new Person("Diego", 10));
+        Node node3 = new Node<Person>(new Person("Pepito", 32));
+        Node node4 = new Node<Person>(new Person("Diego", 31));
         node1.setNext(node2);
         node2.setNext(node3);
         node3.setNext(node4);
-        System.out.println(node1.toString()
-    );
+        System.out.println(node1.toString());
+        runStackExample();
     }
+    public static void runStackExample(){
+            Stack<Person> stack = new Stack<Person>();
+            stack.push(new Person("Juan", 30));
+            stack.push(new Person("Maria", 35));
+            stack.push(new Person("Pepito", 32));
+            stack.push(new Person("Diego", 31));
+
+            System.out.println("Tamaño: " + stack.size());
+            stack.printAllNodes();
+            System.out.println("POP: " + stack.size());
+            System.out.println("Size= " + stack.size());
+            stack.printAllNodes();
+        }
 }
---
+++ src/controller/Stack.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.util.EmptyStackException;
+import models.Node;
+public class Stack<T> {
+    private Node<T>  top;
+    private int size;
+
+    public Stack(){
+        this.top = null;
+        this.size = 0;
+        
+    }
+
+    //Metodo para insertar
+    public void push(T data){
+        Node<T> newNode = new Node<T>(data);
+        newNode.setNext(top);
+        top = newNode;
+        size++; 
+    }
+
+    //Metodo para sacar y/o ekiminar
+    public T pop(){
+        
+        if(top == null){
+            return null;
+            //throw new EmptyStackException(); //es como programar un error en rojo y e mas eficiente 
+        }
+        T data = top.getValue();
+        top = top.getNext();
+        size--;
+        return data;
+    }
+
+
+    public T peek(){
+        if(top == null)
+            //return null;
+            throw new EmptyStackException(); //es como programar un error en rojo y e mas eficiente 
+        return top.getValue();
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public int size(){
+        if(isEmpty()){
+            return 0;
+        }
+        return size;
+    }
+
+    public void printAllNodes(){
+        Node<T> current = top;
+        System.out.println("Clima -> ");
+        while(current != null){
+            System.out.println(current.getValue()+ "_->") ;
+            current = current.getNext();
+        }
+        System.out.println("Fin!!!!!");
+    }
+}
